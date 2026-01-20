da2fe0e3321b5a2c2f2492f6ef24107e2dfb2f63
Denis Kravets <krde0503@gmail.com>
2026-01-20 23:52:22 +0100

    create application

---
+++ GpsTracker/Views/GpsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GpsTrackerApp.swift
+//  GpsTracker
+//
+//  Created by Denis Kravets on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct GpsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TrackerListView()
+        }
+    }
+}
---
+++ GpsTracker/Views/TrackerListView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackerListView.swift
+//  GpsTracker
+//
+//  Created by Denis Kravets on 20.01.2026.
+//
+
+import SwiftUI
+
+struct TrackerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackerListView()
+}
